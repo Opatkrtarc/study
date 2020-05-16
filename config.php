f1901fe832d80cbf9a11a87aa824a3ec66dea53f
Opatkrtarc <qyf1991523460@163.com>
2020-05-16 10:48:11 +0800

    init application

--- config.php
+++ config.php
@@ -1,4 +1,7 @@
 <?php
 $cfg['site']='sssss';
+$cfg['db']['host']='localhost';
+$cfg['db']['user']='devel';
+$cfg['db']['user']='123456';
 
 
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+require('config.php');
 load_config();
 run_app();
 
